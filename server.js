ccbf67cc9eddf5fbf89660571b7bc6109fa57632
Vikky1991 <vikrantsaini02@gmail.com>
2026-04-01 08:52:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Serve static files
+app.use(express.static(__dirname));
+
+// Home route → index.html
 app.get("/", (req, res) => {
-  res.send("Server is live 🚀");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
