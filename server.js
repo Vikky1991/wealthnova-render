c6daad68df1403075e3d35fc0f5ee350303345ee
Vikky1991 <vikrantsaini02@gmail.com>
2026-04-01 07:46:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 const express = require("express");
 const app = express();
 
